d3fd6ab1125afd560e38fcc7934e927055538cea
vishwa6509 <kidommmunited@gmail.com>
2025-02-23 09:50:50 +0530

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(cors());
 app.set("view engine", "ejs");
 
 //Modify your URL here
-var hostURL="YOUR URL";
+var hostURL="https://replit.com/@americanmamm/TrackDown-1#README.md";
 //TOGGLE for Shorters
 var use1pt=false;
 
