5bb1b9725d6dafc681300cc1411017dc8873dd90
vishwa6509 <kidommmunited@gmail.com>
2025-03-08 15:34:19 +0530

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(cors());
 app.set("view engine", "ejs");
 
 //Modify your URL here
-var hostURL="https://trackdown-pq0q.onrender.com/";
+var hostURL="https://trackdown-pq0q.onrender.com";
 //TOGGLE for Shorters
 var use1pt=false;
 
